1230412a8de159db3cc061e04cdeb6452efe5b4d
Yuri <yuri_geovani@hotmail.com>
2020-12-22 22:57:39 +0000

    Until class 88

---
+++ 5. Javascript - Função/86. IIFE/iife.js
@@ -0,0 +1,8 @@
+// IIFE -> Immediately Invoked Function Expression
+
+(function(){
+    console.log('Será executado na hora!');
+    console.log('Foge do escopo mais abrangente!')
+})();
+
+// Uma forma de executar imediatamente e fugindo do escopo global
