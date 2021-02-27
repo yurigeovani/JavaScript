362c4b341b6a429d5395568c717de6e4307aad82
Yuri <yuri_geovani@hotmail.com>
2021-02-27 22:42:11 +0000

    Until class 124, exercise 18

--- 8. Exercícios/18.js
+++ 8. Exercícios/18.js
@@ -11,3 +11,24 @@ despesasTotais([
 {nome: "Galaxy S20", categoria: "Eletrônicos", preco: 3599.99},
 {nome: "Macbook Pro", categoria: "Eletrônicos", preco: 30999.90}
 ]) // retornará 34599.89 */
+
+function despesasTotais(despesa){
+    let resultado = 0;
+
+    for(let i of despesa){
+        resultado+=i.preco;
+    }
+
+    return console.log(resultado)
+}
+
+
+
+despesasTotais([
+    {nome: "Jornal online", categoria: "Informação", preco: 89.99},
+    {nome: "Cinema", categoria: "Entretenimento", preco: 150}
+]) // retornará 239.99
+despesasTotais([
+    {nome: "Galaxy S20", categoria: "Eletrônicos", preco: 3599.99},
+    {nome: "Macbook Pro", categoria: "Eletrônicos", preco: 30999.90}
+]) // retornará 34599.89
