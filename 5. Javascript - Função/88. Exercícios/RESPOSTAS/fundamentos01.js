0daa290bdf0bd74d24b88d2171dd2acbec30ff2d
Yuri <yuri_geovani@hotmail.com>
2021-03-20 01:09:16 +0000

    Until class 145

--- 5. Javascript - Função/88. Exercícios/RESPOSTAS/fundamentos01.js
+++ 5. Javascript - Função/88. Exercícios/RESPOSTAS/fundamentos01.js
@@ -1,5 +1,5 @@
 function calcularOperacoes (operador1, operador2) {
-    console.log(operador1+operador2, operador1-operador2, operador1*operador2, operador1/operador2);
+    return console.log(operador1+operador2, operador1-operador2, operador1*operador2, operador1/operador2);
 }
 
 calcularOperacoes(1, 2)
